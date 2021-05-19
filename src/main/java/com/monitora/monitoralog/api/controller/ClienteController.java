1db9d97f4f6dc06c2498dabbc552084ef0edd886
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-05-19 17:22:05 -0300

    Banco e configurações de acesso ao banco MySql criados. Repositório para Entidade de Cliente criado. Rotas de buscar client por id, criar clientes, atualizar cliente por id e deletar cliente por id feitos

--- src/main/java/com/monitora/monitoralog/api/controller/ClienteController.java
+++ src/main/java/com/monitora/monitoralog/api/controller/ClienteController.java
@@ -1,30 +1,86 @@
 package com.monitora.monitoralog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.monitora.monitoralog.api.domain.model.Cliente;
+import com.monitora.monitoralog.api.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+	
+//	@PersistenceContext
+//	private EntityManager manager;
+	
+	@Autowired
+	private ClienteRepository clientRepository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("85 30324715");
-		cliente1.setEmail("joao@monitoratec.com");
+		return clientRepository.findAll();
+	
+//		return manager.createQuery("from Cliente", Cliente.class)
+//				.getResultList();
+//	}
+	}
+	
+	@GetMapping("/{clientId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clientId) {
+		return clientRepository.findById(clientId)
+				.map(cliente -> ResponseEntity.ok(cliente))
+				.orElse(ResponseEntity.notFound().build());
 		
-		var cliente2 = new Cliente();
-		cliente2.setId(1L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("85 999955681");
-		cliente2.setEmail("maria@monitoratec.com");
+//		Optional<Cliente> cliente = clientRepository.findById(clientId)
+//		if(cliente.isPresent()) {
+//			return ResponseEntity.ok(cliente.get());
+//		}
+//		
+//		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clientRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clientId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clientId, @RequestBody Cliente cliente){
+		if(!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
 		
-		return Arrays.asList(cliente1, cliente2);
+		cliente.setId(clientId);
+		cliente = clientRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clientId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clientId){
+		if(!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clientRepository.deleteById(clientId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
